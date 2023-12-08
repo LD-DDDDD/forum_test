558b371d4609bef4b470e1c8bbcf001e27cbfbe3
Лу Жуньда <ld.anton617@gmail.com>
2023-12-08 04:12:08 +0300

    管理员可登录

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path, re_path
 from . import views
 
+
 urlpatterns = [
     path(r'', views.post_list, name='post_list'),
     re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     re_path(r'^post/new/$', views.post_new, name='post_new'),
     re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
@@ -12,6 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'forum/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +27,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'forum/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,3 +41,28 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'forum/post_edit.html', {'form': form})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
